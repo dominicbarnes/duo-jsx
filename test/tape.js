0e522626211a18e19a0e815dbf5c4c9bcce3c077
Evan Mark Hopkins <evanmarkhopkins@gmail.com>
2015-03-21 04:46:06 -0500

    fix typos

--- test/tape.js
+++ test/tape.js
@@ -14,29 +14,27 @@ test('Testing plugin', function (t) {
     t.plan(7);
 
     // should compile a sring without errors
-    var duo0 = new Duo(__dirname);
-    duo0.entry(file, 'jsx')
-    duo0.use(jsx())
-    duo0.run(function (err, src) {
+    var duo = new Duo(__dirname);
+    duo.entry(file, 'jsx')
+    duo.use(jsx())
+    duo.run(function (err, src) {
         t.error(err, 'should compile .jsx');
         t.equal(typeof src.code, 'string');
         t.ok(src.code.match('createElement'), 'confirm output');
     });
 
     // should compile a sring without errors
-    var duo0 = new Duo(__dirname);
-    duo0.entry(file, 'js')
-    duo0.use(jsx())
-    duo0.run(function (err, src) {
+    duo.entry(file, 'js')
+    duo.use(jsx())
+    duo.run(function (err, src) {
         t.error(err, 'should compile .js');
         t.ok(src.code.match('createElement'), 'confirm output');
     });
     
     //should skip non-jsx
-    var duo2 = new Duo(__dirname);
-    duo2.entry(file, 'css')
-    duo2.use(jsx())
-    duo2.run(function (err, src) {
+    duo.entry(file, 'css')
+    duo.use(jsx())
+    duo.run(function (err, src) {
         t.error(err, 'should skip css');
         t.ok(src.code.match('<span>'), 'confirm output');
     });

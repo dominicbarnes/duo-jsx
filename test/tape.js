2920e54e5f2b20b082700a59b4ac1514f7412c2d
Evan Mark Hopkins <evanmarkhopkins@gmail.com>
2015-03-24 22:30:19 -0500

    tinkering

--- test/tape.js
+++ test/tape.js
@@ -1,40 +1,28 @@
 var Duo = require('duo');
-var jsx = require('../lib/index');
+var jsx = require('../lib');
 var test = require('tape');
 
-var file = "var Class = React.createClass({  \n" +
-           "    render: function () {        \n" +
-           "        return (                 \n" +
-           "            <span>testing</span> \n" +
-           "        );                       \n" +
-           "    }                            \n" +
-           "});                              \n" ;
+var file = "React.render(<span/>, document.querySelector('body'));";
 
 test('Testing plugin', function (t) {
     t.plan(6);
 
     // should compile jsx without errors
     var duo = new Duo(__dirname);
-    duo.entry(file, 'jsx')
-    duo.use(jsx())
-    duo.run(function (err, src) {
+    duo.entry(file, 'jsx').use(jsx()).run(function (err, src) {
         t.error(err, 'should compile .jsx');
-        t.ok(src.code.match('createElement'), 'confirm output');
+        t.ok(src.code.match('createElement'), 'confirm transformation');
     });
 
     // should compile js without errors
-    duo.entry(file, 'js')
-    duo.use(jsx())
-    duo.run(function (err, src) {
+    duo.entry(file, 'js').use(jsx()).run(function (err, src) {
         t.error(err, 'should compile .js');
-        t.ok(src.code.match('createElement'), 'confirm output');
+        t.ok(src.code.match('createElement'), 'confirm transformation');
     });
 
     //should skip non-jsx
-    duo.entry(file, 'css')
-    duo.use(jsx())
-    duo.run(function (err, src) {
+    duo.entry(file, 'css').use(jsx()).run(function (err, src) {
         t.error(err, 'should skip css');
-        t.ok(src.code.match('<span>'), 'confirm output');
+        t.ok(src.code.match('<span/>'), 'confirm skip');
     });
 });
